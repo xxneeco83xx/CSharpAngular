6be94b8829d96b2e6f47463c6e3b97306913de53
nbuzzi <nbuzzi@GOD-WS-200>
2016-04-27 16:53:22 -0300

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public string getUsers()
+        {
+            IList<User> userList = new List<User> { new User { ID = 1, Nombre = "Bruno", Apellido = "Tello" },
+                new User { ID = 2, Nombre = "Nicolas", Apellido = "Buzzi" } };
+
+            JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+            return javaScriptSerializer.Serialize(userList);
+        }
+
+    }
+}
---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class User
+{
+    public int ID { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+}
